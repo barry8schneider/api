314b5aedc862dfb42011e225174d64b7bcaa25ef
Jay Hariani <jay@govtribe.com>
2014-05-10 16:53:15 -0400

    Added average award values to all entities.

--- app/lib/GovTribe/Models/APIEntity.php
+++ app/lib/GovTribe/Models/APIEntity.php
@@ -54,16 +54,138 @@ class APIEntity extends \Jenssegers\Mongodb\Model {
 
 		$agg = \DB::connection('graph')->getCollection('edges')->aggregate($pipeline);
 
-		$timeBuckets = [];
+		$output = [];
 
 		if (isset($agg['result']))
 		{
 			foreach ($agg['result'] as $year)
 			{
-				$timeBuckets[] = ['calendarYear' => substr($year['_id'], -8, 4), 'numberOfAwards' => $year['c']];
+				$output[] = ['calendarYear' => substr($year['_id'], -8, 4), 'numberOfAwards' => $year['c']];
 			}
 		}
 
-		return $timeBuckets;
+		return $output;
 	}
+
+	protected function getAverageAwardValuesAttribute()
+	{
+		$pipeline = [
+			['$match' => [
+				'_id' => new \MongoRegex(
+					'/^' . strtolower(class_basename($this)) . '\\|' . $this->_id . '\\|project\\|project\\|wf\\|awarded/'
+					)
+				]
+			],
+			['$match' => ['ts.av' => ['$exists' => true]]],
+			['$project' => ['ts.tb' => 1, 'ts.av' => 1]],
+			['$unwind' => '$ts'],
+			['$unwind' => '$ts.tb'],
+			['$match' => ['ts.tb' => new \MongoRegex('/year/')]],
+			['$group' => ['_id' => '$ts.tb', 'avg' => ['$avg' => '$ts.av']]],
+			['$sort' => ['_id' => -1]],
+		];
+
+		$agg = \DB::connection('graph')->getCollection('edges')->aggregate($pipeline);
+
+		$output = [];
+
+		if (isset($agg['result']))
+		{
+			foreach ($agg['result'] as $year)
+			{
+				$output[] = ['calendarYear' => substr($year['_id'], -8, 4), 'averageAwardValue' => round($year['avg'], 2)];
+			}
+		}
+
+		return $output;
+	}
+
+	protected function getActivePeopleAttribute()
+	{
+		$pipeline = [
+			['$match' => [
+				'_id' => new \MongoRegex(
+					'/^' . strtolower(class_basename($this)) . '\\|' . $this->_id . '\\|person\\|project\\|tb\\|.*year/'
+					)
+				]
+			],
+			['$unwind' => '$ts'],
+			['$group' => ['_id' => '$_id', 'c' => ['$sum' => 1]]],
+			['$sort' => ['_id' => -1]],
+		];
+
+		$agg = \DB::connection('graph')->getCollection('edges')->aggregate($pipeline);
+
+		$output = [];
+
+		if (isset($agg['result']))
+		{
+			foreach ($agg['result'] as $year)
+			{
+				$output[] = ['calendarYear' => substr($year['_id'], -8, 4), 'numberOfActivePeople' => $year['c']];
+			}
+		}
+
+		return $output;
+	}
+
+	protected function getActiveOfficesAttribute()
+	{
+		$pipeline = [
+			['$match' => [
+				'_id' => new \MongoRegex(
+					'/^' . strtolower(class_basename($this)) . '\\|' . $this->_id . '\\|office\\|project\\|tb\\|.*year/'
+					)
+				]
+			],
+			['$unwind' => '$ts'],
+			['$group' => ['_id' => '$_id', 'c' => ['$sum' => 1]]],
+			['$sort' => ['_id' => -1]],
+		];
+
+		$agg = \DB::connection('graph')->getCollection('edges')->aggregate($pipeline);
+
+		$output = [];
+
+		if (isset($agg['result']))
+		{
+			foreach ($agg['result'] as $year)
+			{
+				$output[] = ['calendarYear' => substr($year['_id'], -8, 4), 'numberOfActiveOffices' => $year['c']];
+			}
+		}
+
+		return $output;
+	}
+
+	protected function getTotalProtestsAttribute()
+	{
+		return;
+		$pipeline = [
+			['$match' => [
+				'_id' => new \MongoRegex(
+					'/^' . strtolower(class_basename($this)) . '\\|' . $this->_id . '\\|protest\\|protest\\|tb\\|.*year/'
+					)
+				]
+			],
+			['$unwind' => '$ts'],
+			['$group' => ['_id' => '$_id', 'c' => ['$sum' => 1]]],
+			['$sort' => ['_id' => -1]],
+		];
+
+		$agg = \DB::connection('graph')->getCollection('edges')->aggregate($pipeline);
+
+		$output = [];
+
+		if (isset($agg['result']))
+		{
+			foreach ($agg['result'] as $year)
+			{
+				$output[] = ['calendarYear' => substr($year['_id'], -8, 4), 'numberOfActiveOffices' => $year['c']];
+			}
+		}
+
+		return $output;
+	}
+
 }
--- app/lib/GovTribe/Transformers/AgencyTransformer.php
+++ app/lib/GovTribe/Transformers/AgencyTransformer.php
@@ -34,11 +34,22 @@ class AgencyTransformer extends Transformer
 					'acronyms' => $entity->acronyms ? $entity->acronyms : self::EMPTY_NTI_ARRAY,
 					'timestamp'  => $entity->timestamp ? Carbon::createFromTimeStamp($entity->timestamp->sec)->toISO8601String() : self::NULL_TIMESTAMP,
 					'website'  => $entity->sourceLink ? $entity->sourceLink : self::NULL_TEXT,
+
 					'procurementStats' => [
 						'awardDollarFlow' => isset($entity->market['dollarFlow']) ? $entity->market['dollarFlow'] : self::EMPTY_NTI_ARRAY,
-						'numbersOfAwards' => $entity->numberOfAwards,
+						'numbersOfAwards' => $entity->numberOfAwards ? $entity->numberOfAwards : self::EMPTY_NTI_ARRAY,
+						'averageAwardValues' => $entity->averageAwardValues ? $entity->averageAwardValues : self::EMPTY_NTI_ARRAY,
 					],
 
+					'organizationalStats' => [
+						'activePeople' => $entity->activePeople ? $entity->activePeople : self::EMPTY_NTI_ARRAY,
+						'activeOffices' => $entity->activeOffices ? $entity->activeOffices : self::EMPTY_NTI_ARRAY,
+					],
+
+					'protestStats' => [
+						'totalProtests' => $entity->totalProtests ? $entity->totalProtests : self::EMPTY_NTI_ARRAY,
+						'protestsWithdrawn' => $entity->protestsWithdrawn ? $entity->protestsWithdrawn : self::EMPTY_NTI_ARRAY,
+					],
 
 				);
 
