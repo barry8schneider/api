74adfed23a09d4afa96506f69260e444ad9c4ba2
Jay Hariani <jay@govtribe.com>
2014-05-21 18:19:25 -0400

    Added a test config file.

---
+++ .env.testing.php
@@ -0,0 +1,37 @@
+<?php
+
+return array(
+
+	'APP_KEY' => 'YourSecretKey!!!',
+
+	'MONGO_HOST_DATABOT' => 'localhost',
+	'MONGO_PORT_DATABOT' => 27017,
+	'MONGO_USERNAME_DATABOT' => null,
+	'MONGO_PASSWORD_DATABOT' => null,
+	'MONGO_DATABASE_DATABOT' => 'databot-test',
+
+	'MONGO_HOST_GRAPH' => 'localhost',
+	'MONGO_USERNAME_GRAPH' => null,
+	'MONGO_PASSWORD_GRAPH' => null,
+	'MONGO_DATABASE_GRAPH' => 'graph-test',
+	'MONGO_RS_GRAPH' => null,
+
+	'ELASTICA_URI' => null,
+
+	'REDIS_USERNAME' => null,
+	'REDIS_PASSWORD' => null,
+	'REDIS_HOST' => 'localhost',
+	'REDIS_PORT' => 6379,
+
+	'KINVEY_APP_NAME' => null,
+	'KINVEY_APP_KEY' => null,
+	'KINVEY_APP_SECRET' => null,
+	'KINVEY_APP_MASTER_SECRET' => null,
+
+	'MANDRILL_USER_NAME' => null,
+	'MANDRILL_API_KEY' => null,
+
+	'RAVEN_DSN' => null,
+
+	'MIXPANEL_PROJECT_TOKEN' => null,
+);
